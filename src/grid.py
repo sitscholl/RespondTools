0cac292ad73a08c0e117bfd58d7ee4655764b599
sitscholl <tscholl.simon@gmail.com>
2026-02-04 10:35:44 +0100

    refactored grid class to use fit_transform() method and TransformationParameters class

--- src/grid.py
+++ src/grid.py
@@ -4,14 +4,45 @@ import numpy as np
 
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Dict
+import logging
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class TransformParameters:
+    data_max: float | int
+    data_min: float | int
+    out_max: float | int
+    out_min: float | int
+    original_dtype: np.dtype
+    new_dtype: np.dtype
+
+    @property
+    def denom(self):
+        denom = self.data_max - self.data_min
+        denom = denom if not np.isclose(denom, 0) else 1
+        return denom
+
+    @property
+    def scale(self):
+        return (self.data_max - self.data_min) / (self.out_max - self.out_min)
+
+    @property
+    def offset(self):
+        return (self.data_min - self.out_min * self.scale)
 
 @dataclass
 class Grid:
-    data: xr.Dataset | xr.DataArray
+    data: xr.Dataset
     original_location: str | Path
-    original_dtype: np.dtype | None = None
-    scale: float | None = None
-    offset: float | None = None
+    nodata_mask: xr.Dataset
+    nodata: float | int | None
+    transformation: Dict[str, TransformParameters] | None = None
+
+    def __post_init__(self):
+        if not isinstance(self.data, xr.Dataset):
+            raise ValueError(f"Data must be an xr.Dataset. Got {type(self.data)}")
 
     @classmethod
     def from_geotiff(cls, path: str | Path):
@@ -19,7 +50,24 @@ class Grid:
         if not path.exists():
             raise ValueError(f"Could not find geotiff file at {path}")
         data = xr.open_dataarray(path).squeeze(drop = True)
-        return cls(data, path)
+
+        if isinstance(data, xr.DataArray):
+            data = data.to_dataset(name = data.name or 'variable')
+
+        nodata_mask = []
+        for var, arr in data.data_vars.items():
+            nodata = getattr(arr.rio, "nodata", None)
+            
+            if nodata is None:
+                if np.issubdtype(arr.dtype, np.float):
+                    nodata_mask.append(xr.where(arr.notnull(), 1, 0).astype(bool))
+                    nodata = np.nan
+                else:
+                    nodata_mask.append(xr.ones_like(arr).astype(bool))
+            else:
+                nodata_mask.append(xr.where(arr != nodata and arr.notnull(), 1, 0).astype(bool))
+
+        return cls(data, path, xr.merge(nodata_mask), nodata)
 
     @property
     def crs(self):
@@ -29,94 +77,100 @@ class Grid:
     def resolution(self):
         return self.data.rio.resolution()
 
-    def scale_array(self, da: xr.DataArray, out_dtype, nodata = None, dim = None) -> xr.DataArray:
+    def fit_transformation(self, out_dtype: np.dtype = np.uint8):
+        
         out_info = np.iinfo(out_dtype)
         out_min, out_max = out_info.min, out_info.max
 
-        valid = da if nodata is None else da.where(da != nodata)
+        transform_dict = {}
+        for var, arr in self.data.data_vars.items():
+            valid = arr.where(self.nodata_mask[var])
 
-        if dim and dim in da.dims:
-            data_min = valid.min(dim=["y", "x"])
-            data_max = valid.max(dim=["y", "x"])
-        else:
             data_min = valid.min()
             data_max = valid.max()
 
-        # Avoid divide-by-zero if data is constant
-        denom = data_max - data_min
-        denom = xr.where(denom == 0, 1, denom)
-
-        scaled = (da - data_min) / denom
-        scaled = scaled * (out_max - out_min) + out_min
-        scaled = scaled.clip(out_min, out_max)
-
-        if np.issubdtype(out_dtype, np.integer):
-            # Avoid invalid casts when NaN/Inf are present.
-            scaled = scaled.where(np.isfinite(scaled), out_min)
-
-        scaled = scaled.astype(out_dtype)
-
-        # Preserve nodata
-        if nodata is not None:
-            scaled = scaled.where(da != nodata, out_min)
-
-        # Store scale/offset as attrs for inversion
-        scaled.attrs["scale"] = (data_max - data_min) / (out_max - out_min)
-        scaled.attrs["offset"] = data_min - out_min * scaled.attrs["scale"]
-        return scaled
-
-    def transform(self, out_dtype = np.uint8, dim = None):
-
-        data = self.data
-
-        # Handle nodata if present via rioxarray
-        nodata = getattr(data.rio, "nodata", None)
-
-        if isinstance(data, xr.Dataset):
-            transformed = data.map(lambda x: self.scale_array(x, out_dtype = out_dtype, nodata = nodata, dim = dim))
-            scale = None
-            offset = None
-        else:
-            transformed = self.scale_array(data, out_dtype = out_dtype, nodata = nodata, dim = dim)
-            scale = transformed.attrs.get("scale")
-            offset = transformed.attrs.get("offset")
+            transformation = TransformParameters(
+                data_max = data_max,
+                data_min = data_min,
+                out_max = out_max,
+                out_min = out_min,
+                original_dtype = arr.dtype,
+                new_dtype = out_dtype
+            )
+            transform_dict[var] = transformation
 
         return Grid(
-            data=transformed,
-            original_location=self.original_location,
-            original_dtype=self.data.dtype,
-            scale = scale,
-            offset = offset
+            data = self.data,
+            original_location = self.original_location,
+            nodata_mask = self.nodata_mask,
+            transformation = transform_dict
         )
 
-    def inverse_transform(self, out_dtype: np.dtype | None = None):
-        data = self.data
-        target_dtype = out_dtype or self.original_dtype
+    def transform(self):
 
-        def _inverse(da: xr.DataArray) -> xr.DataArray:
-            scale = da.attrs.get("scale", self.scale)
-            offset = da.attrs.get("offset", self.offset)
-            if scale is None or offset is None:
-                raise ValueError("Missing scale/offset for inverse_transform.")
-            restored = da.astype(np.float64) * scale + offset
-            if target_dtype is not None and np.issubdtype(target_dtype, np.integer):
-                restored = restored.where(np.isfinite(restored), 0)
-            if target_dtype is not None:
-                restored = restored.astype(target_dtype)
-            return restored
+        scaled_arrays = []
+        for var, arr in self.data.data_vars.items():
 
-        if isinstance(data, xr.Dataset):
-            restored = data.map(_inverse)
-        else:
-            restored = _inverse(data)
+            if var not in self.transformation.keys():
+                logger.warning(f'No transformation stored for variable {var}, Will be skipped')
+                continue
 
+            transform = self.transformation[var]
+
+            scaled = (arr - transform.data_min) / transform.denom
+            scaled = scaled * (transform.out_max - transform.out_min) + transform.out_min
+            scaled = scaled.clip(transform.out_min, transform.out_max)
+
+            if np.issubdtype(transform.new_dtype, np.integer):
+                # Avoid invalid casts when NaN/Inf are present.
+                scaled = scaled.where(np.isfinite(scaled), transform.out_min)
+
+            scaled = scaled.astype(transform.new_dtype)
+
+            # Preserve nodata
+            scaled = scaled.where(self.nodata_mask[var], transform.out_min)
+
+        scaled_ds = xr.merge(scaled_arrays)
         return Grid(
-            data=restored,
-            original_location=self.original_location,
-            original_dtype=target_dtype,
-            scale=None,
-            offset=None,
+            data = scaled_ds,
+            original_location = self.original_location,
+            transformation = {i:j for i,j in self.transformation.items() if i in scaled_ds.data_vars}
         )
+
+    # def inverse_transform(self, out_dtype: np.dtype | None = None):
+    #     data = self.data
+    #     target_dtype = out_dtype or self.original_dtype
+    #     nodata = self.nodata
+    #     if nodata is None:
+    #         nodata = getattr(data.rio, "nodata", None)
+
+    #     def _inverse(da: xr.DataArray) -> xr.DataArray:
+    #         scale = da.attrs.get("scale", self.scale)
+    #         offset = da.attrs.get("offset", self.offset)
+    #         if scale is None or offset is None:
+    #             raise ValueError("Missing scale/offset for inverse_transform.")
+    #         restored = da.astype(np.float64) * scale + offset
+    #         if target_dtype is not None and np.issubdtype(target_dtype, np.integer):
+    #             restored = restored.where(np.isfinite(restored), 0)
+    #         if target_dtype is not None:
+    #             restored = restored.astype(target_dtype)
+    #         if nodata is not None:
+    #             restored = restored.where(np.isfinite(restored), nodata)
+    #         return restored
+
+    #     if isinstance(data, xr.Dataset):
+    #         restored = data.map(_inverse)
+    #     else:
+    #         restored = _inverse(data)
+
+    #     return Grid(
+    #         data=restored,
+    #         original_location=self.original_location,
+    #         original_dtype=target_dtype,
+    #         scale=None,
+    #         offset=None,
+    #         nodata=nodata,
+    #     )
 
     def align(self, *others):
         pass
