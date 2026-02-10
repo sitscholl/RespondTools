52e5dee513278a21d76831973f37efa6c5fcb847
sitscholl <tscholl.simon@gmail.com>
2026-02-10 11:46:34 +0100

    implement GridResolution for resolution handling

--- src/grid_loader.py
+++ src/grid_loader.py
@@ -4,19 +4,91 @@ from odc.geo.xr import xr_reproject
 
 from pathlib import Path
 import logging
-from pyproj import CRS, Geod
+from pyproj import CRS, Geod, Transformer
+from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
+@dataclass
+class GridResolution:
+    res: float
+    crs: CRS
+    unit: str
+    reference_latitude: float
+
+    @classmethod
+    def from_array(cls, array: xr.DataArray | xr.Dataset):
+        res = np.min(np.abs(array.rio.resolution())).item()
+        crs = CRS.from_user_input(array.rio.crs)
+        unit = 'm' if crs.is_projected else 'deg'
+        reference_latitude = cls._reference_lat(array)
+
+        return cls(
+            res = res,
+            crs = crs,
+            unit = unit,
+            reference_latitude = reference_latitude
+        )
+
+    def to_deg(self, orig_res: float | None = None):
+        """Transform resolution in m to degrees using dataset center latitude."""
+
+        if orig_res is None and self.unit == 'deg':
+            return self.res
+
+        orig_res = self.res if orig_res is None else orig_res
+
+        ref_lat = self.reference_latitude
+        geod = Geod(ellps="WGS84")
+        meters_per_degree = geod.inv(0.0, ref_lat, 1.0, ref_lat)[2]
+        return orig_res / meters_per_degree
+
+    def to_m(self, orig_res: float | None = None):
+        """Transform resolution in degrees to meters using dataset center latitude."""
+
+        if orig_res is None and self.unit == 'm':
+            return self.res
+
+        orig_res = self.res if orig_res is None else orig_res
+
+        ref_lat = self.reference_latitude
+        geod = Geod(ellps="WGS84")
+        meters_per_degree = geod.inv(0.0, ref_lat, 1.0, ref_lat)[2]
+        return orig_res * meters_per_degree
+
+    @staticmethod
+    def _reference_lat(data: xr.DataArray | xr.Dataset) -> float:
+        """Return center latitude in degrees, transforming bounds if needed."""
+        try:
+            minx, miny, maxx, maxy = data.rio.bounds()
+            data_crs = CRS.from_user_input(data.rio.crs)
+            if data_crs.is_projected:
+                transformer = Transformer.from_crs(data_crs, CRS.from_epsg(4326), always_xy=True)
+                _, miny = transformer.transform(minx, miny)
+                _, maxy = transformer.transform(maxx, maxy)
+            ref_lat = (miny + maxy) / 2.0
+            return float(ref_lat)
+        except Exception as e:
+            logger.exception(f"Faild to compute reference latitude. Using latitude of 0 as default. Error: {e}")
+            return 0.0
+
 class GridLoader:
 
-    def __init__(self, max_resolution: float = 100, target_crs: str = "4326", resampling_method: str = 'bilinear'):
+    def __init__(
+        self, 
+        max_resolution: float = 100, 
+        target_crs: str = "4326", 
+        resampling_method: str = 'bilinear',
+        filesize_threshold: int = 1_000_000
+        ):
+
         self.max_resolution_m = max_resolution
         try:
             self.target_crs = CRS.from_user_input(target_crs)
         except Exception as exc:
             raise ValueError(f"Invalid target_crs: {target_crs}") from exc
         self.resampling_method = resampling_method
+        self.filesize_threshold = filesize_threshold
 
         self.files = []
 
@@ -38,7 +110,13 @@ class GridLoader:
         if not file.exists():
             raise ValueError(f"Could not find file at {file}")
 
-        data = xr.open_dataset(file)
+        filesize = file.stat().st_size
+        if filesize > self.filesize_threshold:
+            chunks = 'auto'
+        else:
+            chunks = None
+
+        data = xr.open_dataset(file, chunks = chunks)
 
         if data.rio.crs is None:
             raise ValueError(f"No crs found for file {file}")
@@ -47,36 +125,41 @@ class GridLoader:
         if len(data_vars) == 1 and data_vars[0] == 'band_data':
             data = data.rename({'band_data': file.stem})
 
-        res = data.rio.resolution()
-        if res is None:
-            raise ValueError(f"Could not get resolution of file {file}")
-
-        max_res = np.min(np.abs(res)).item()
-        is_projected = data.rio.crs.is_projected
-        if is_projected:
+        grid_res = GridResolution.from_array(data)
+        if grid_res.unit == 'm':
             allowed_max_res = self.max_resolution_m
         else:
-            allowed_max_res = self._transform_resolution(self.max_resolution_m, data)
+            allowed_max_res = grid_res.to_deg(self.max_resolution_m)
 
         data_crs = CRS.from_user_input(data.rio.crs)
-        if (max_res < allowed_max_res) or data_crs != self.target_crs:
-            data = self._resample_and_reproject(data, allowed_max_resolution = allowed_max_res)
+        if (grid_res.res < allowed_max_res) or data_crs != self.target_crs:
+            data = self._resample_and_reproject(data, grid_res = grid_res, allowed_max_resolution = allowed_max_res)
 
         return data
 
-    def _resample_and_reproject(self, data: xr.DataArray | xr.Dataset, allowed_max_resolution: float):
+    def _resample_and_reproject(self, data: xr.DataArray | xr.Dataset, grid_res: GridResolution, allowed_max_resolution: float):
         """Change resolution of data to taret_res"""
 
-        res = data.rio.resolution()
-        max_res = np.min(np.abs(res)).item()
+        resolution_units_stay = (
+            (data.rio.crs.is_projected and self.target_crs.is_projected)
+            or (not data.rio.crs.is_projected and not self.target_crs.is_projected)
+        )
 
-        if max_res >= allowed_max_resolution:
-            target_res = 'same'
+        if grid_res.res >= allowed_max_resolution:
+            if resolution_units_stay:
+                target_res = "same"
+            elif self.target_crs.is_projected:
+                target_res = grid_res.to_m()
+            else:
+                target_res = grid_res.to_deg()
         else:
-            target_res = allowed_max_resolution
+            if self.target_crs.is_projected:
+                target_res = grid_res.to_m(allowed_max_resolution)
+            else:
+                target_res = grid_res.to_deg(allowed_max_resolution)
 
         logger.info(
-            f"Resampling data from crs {data.rio.crs.to_epsg()} and resolution {max_res} "
+            f"Resampling data from crs {data.rio.crs.to_epsg()} and resolution {grid_res.res} "
             f"to crs {self.target_crs.to_epsg()} and resolution {target_res}"
         )
         return xr_reproject(
@@ -85,15 +168,3 @@ class GridLoader:
             resampling=self.resampling_method,
             resolution=target_res
         )
-
-    def _transform_resolution(self, resolution_m, data: xr.DataArray | xr.Dataset):
-        """Transform resolution in m to degrees using dataset center latitude."""
-        try:
-            minx, miny, maxx, maxy = data.rio.bounds()
-            ref_lat = (miny + maxy) / 2.0
-        except Exception:
-            ref_lat = 0.0
-
-        geod = Geod(ellps="WGS84")
-        meters_per_degree = geod.inv(0.0, ref_lat, 1.0, ref_lat)[2]
-        return resolution_m / meters_per_degree
